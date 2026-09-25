453f4297eb3a9b5552c13c5c56973b88e42b54a0
ooaza7769-tech <ooaza7769@gmail.com>
2026-09-25 14:35:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // ====== KONFIGURACJA — UZUPEŁNIJ SWOIMI DANYMI ======
 // 1) Adres i klucz "anon" znajdziesz w Supabase: Project Settings -> API
-const SUPABASE_URL = "https://tkgypoyfgvbmlkryimzd.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://tkgypoyfgvbmlkryimzd.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrZ3lwb3lmZ3ZibWxrcnlpbXpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzMzI1MTcsImV4cCI6MjEwNTkwODUxN30.cS5MCl-SxuaIOJZyzpGnpWv4RG8x-saE1ENMShNMgek";
 
 // Limity — możesz zmienić, ale musisz też zaktualizować CHECK w supabase-schema.sql
