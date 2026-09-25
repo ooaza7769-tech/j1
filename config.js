d3aeff613769ac26af7a0f5ac8062b1215e4372b
ooaza7769-tech <ooaza7769@gmail.com>
2026-09-25 14:00:26 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const SUPABASE_URL = "https://tkgypoyfgvbmlkryimzd.supabase.co/rest/v1/";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrZ3lwb3lmZ3ZibWxrcnlpbXpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAzMzI1MTcsImV4cCI6MjEwNTkwODUxN30.cS5MCl-SxuaIOJZyzpGnpWv4RG8x-saE1ENMShNMgek";
 
 // 2) Site key z Cloudflare Turnstile: dash.cloudflare.com -> Turnstile -> Add site
-const TURNSTILE_SITE_KEY = "0x4AAAAAAFDTcFm6sp-cterh";
+const TURNSTILE_SITE_KEY = "0x4AAAAAAFDTvDQwYWjiy_Vs";
 
 // Limity — możesz zmienić, ale musisz też zaktualizować CHECK w supabase-schema.sql
 const MAX_NICK_LEN = 14;
